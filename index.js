4de886f49dbf675c914b58d72a29d52282414311
Lukas Gaucas <34185919+projektorius96@users.noreply.github.com>
2023-06-08 22:02:29 +0100

    whitespace function added p2.1

--- index.js
+++ index.js
@@ -7,6 +7,33 @@ function isEmpty(path) {
     return readdirSync(path).length === 0; /* most of the times should return false i.e. notEmpty */
 }
 
+
+/* // USAGE:
+> the following shows how to eliminate whitespace within function call (at runtime)
+    * `abc${whitespace(0)}def`  := 'abcdef'
+    * `abc${whitespace(4, "")}def`  := 'abcdef'
+    * `abc${whitespace(0, "", " ")}def` := 'abc def'
+    * `abc${whitespace(6)}def` := 'abc      def'
+    * `abc${whitespace(2, ...[], " ")}def` || `abc${whitespace(2, undefined, "")}def` := 'abc  def'
+    * `abc${whitespace(8, "_")}def` := 'abc________def'
+*/
+function whitespace(n, currentValue = "\xa0", init = ""){
+    // return [...Array(n).fill(currentValue)].map((value, index)=>{
+    //     let acc = "";
+    //     acc += value;
+    //     if (index === n-1){
+    //         return str;
+    //     }
+    // })
+
+    // instead optimise the returning code by leveraging built-in reducer pattern
+    return [...Array(n).fill(currentValue)].reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        init
+    );
+}
+
+
 const initPath = process.argv[2] || "."; // USAGE # node script_file starting_position_to_scan_at root_namespace__default=cwd e.g. node index.js ./ .
 const ROOT = process.argv[3] || process.cwd().split(sep).pop();
 // - credits to @https://stackoverflow.com/questions/5827612/node-js-fs-readdir-recursive-directory-search/66238097#66238097|by:Kabeer+Jaffri
@@ -37,7 +64,7 @@ const getDirRecursive = (initPath) => {
 }
 
     getDirRecursive(initPath).forEach((currentDirentEntry, currentDirentIndex)=>{
-        console.log(currentDirentEntry);
+        /* console.log(currentDirentEntry); */
         const levels = currentDirentEntry.levels;
         const depth = levels.length;
         const relativePath = "."; // @https://learn.microsoft.com/en-gb/windows/win32/fileio/naming-a-file?redirectedfrom=MSDN#naming-conventions
