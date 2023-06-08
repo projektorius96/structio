43439edd4a9b0ce1991cda466c274a02b42bd929
Lukas Gaucas <34185919+projektorius96@users.noreply.github.com>
2023-06-08 20:09:16 +0100

    switch statement for debugging

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const { readdirSync} = require('node:fs');
+const { log } = require('node:console');
+const { readdirSync } = require('node:fs');
 const { EOL } = require('node:os');
-const { sep } = require("node:path");
+const { sep, normalize } = require("node:path");
+
+function isEmpty(path) {
+    return readdirSync(path).length === 0; /* most of the times should return false i.e. notEmpty */
+}
 
 const initPath = process.argv[2] || "."; // USAGE # node script_file starting_position_to_scan_at root_namespace__default=cwd e.g. node index.js ./ .
 const ROOT = process.argv[3] || process.cwd().split(sep).pop();
@@ -10,61 +15,52 @@ const getDirRecursive = (initPath) => {
     try {
         const items = readdirSync(initPath, {withFileTypes: true}); /* console.log("items=?", items); */
         for (const item of items) {
-            if (false){
-                /* ... */
-            }
+            if (!true); 
             else if ( item.isDirectory() ){
                 /* console.log("isDirectory=?", `${item.path}${sep}${item.name}`); */
                 files = [...files, ...getDirRecursive(`${item.path}${sep}${item.name}`)];
+                // if (!isEmpty(normalize(item.path))) {
+                //     files = [...files, {file: item.name, path: normalize(`${item.path}${sep}${item.name}`), levels: item.path.split(sep)/* , noFiles: item.path.split(sep) */} ]
+                // }
             }
             else if (item.isFile()) {
                 /* console.log("isFile=?", `${item.path}${sep}${item.name}`); */
-                files = [...files, {file: item.name, path: `${item.path}${sep}${item.name}`, levels: item.path.split(sep)}];
+                // if (normalize(item.path).split(sep).length > 1) console.log("notEmptyDir=?", item);
+                files = [...files, {file: item.name, path: normalize(`${item.path}${sep}${item.name}`), levels: item.path.split(sep)/* , noFiles: [] */}];
             }
-            else {
-                /* ... */
-            }
-
+            else;
         }
         return files;
     } catch (e) {
         return e;
     }
 }
-    console.log( getDirRecursive(initPath))
-    getDirRecursive(initPath).forEach((entry, index)=>{
-        /* console.log(entry); */
-        const depth = entry.levels.length;
-        const filename = entry.file;
-        if (index === 0){
+
+    getDirRecursive(initPath).forEach((currentDirentEntry, currentDirentIndex)=>{
+        /* console.log(currentDirentEntry); */
+        const depth = currentDirentEntry.levels.length;
+        const filename = currentDirentEntry.file;
+        if (currentDirentIndex === 0 /* to control root level print */){
             process.stdout.write(ROOT);
             process.stdout.write(EOL);
-            process.stdout.write(sep + entry.levels.slice(1, entry.levels.length).join(sep) /* + "\r\n\t" */ + filename);
-            process.stdout.write(EOL);
         }
-        if (index > 0 /* === isDirectory */){
-            if (depth === 1) {
-                process.stdout.write(sep + entry.levels.slice(1, entry.levels.length).join(sep) /* + "\r\n\t" */ + filename);
-                process.stdout.write(EOL);
-            }
-            else {
-                process.stdout.write(sep + entry.levels.slice(1, entry.levels.length).join(sep) + sep /* + "\r\n\t" */ + filename);
-                process.stdout.write(EOL);
-            }
+        // switch drawback is that depth must be know ahead of time (script must be modified to meet the needs)
+        switch(depth){
+            // later we would use depth as multiplier for multiple tabs to avoid semi-automatic switch statement, now leaving as is for debugging purposes:
+            case 1:
+                process.stdout.write(filename)
+                process.stdout.write(EOL)
+            break;
+            case 2:
+                process.stdout.write(`\t${currentDirentEntry.levels.slice(1, currentDirentEntry.levels.length).join(sep) + sep + filename}`)
+                process.stdout.write(EOL)
+            break;
+            case 3:
+                process.stdout.write(`\t\t${currentDirentEntry.levels.slice(1, currentDirentEntry.levels.length).join(sep) + sep + filename}`)
+                process.stdout.write(EOL)
+            break;
+            DEFAULT:;
         }
-        // entry.levels.forEach((pathspec, currentDepth)=>{
-        //     console.log("pathspec", entry.levels);
-        //     if (false){/* ... */}
-        //     else if (depth === 1 /* === isFile */) {
-        //         process.stdout.write(`depth:===${currentDepth}===${pathspec}` + filename);
-        //         process.stdout.write(EOL);
-        //     }
-        //     else if (depth > 1 /* === isDirectory */) {
-        //         process.stdout.write(`depth:===${currentDepth}===${pathspec}`);
-        //         process.stdout.write(EOL);
-        //     }
-        //     else {/* ... */}
-        // })
     })
 
 
